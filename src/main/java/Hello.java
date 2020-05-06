d0a929d4b384719e81e3b75d5a17b75d24603037
Maxim <githubmax@ukr.net>
2020-05-06 15:28:53 +0300

    Added comments for javadoc

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -1,9 +1,30 @@
+import org.jetbrains.annotations.Contract;
+import org.jetbrains.annotations.NotNull;
+
+/**
+ * Class for return text message.
+ */
 public class Hello {
-    public static String hello(){
+    /**
+     * method return "Hello".
+     * @return "Hello"
+     */
+    @org.jetbrains.annotations.NotNull
+    public static String helo() {
+        System.out.println("kk");
         return "Hello";
     }
-    public static String helloAge(int age){
-        if (age <= 0) {throw new  IllegalArgumentException();}
+
+    /**
+     * method return "Hello "age"".
+     * @param age - this is age
+     * @return "Hello "age""
+     */
+    @NotNull
+    @Contract(pure = true)
+    public static String helloAge(int age) {
+        if (age <= 0) {throw new IllegalArgumentException();}
         return "Hello, " + age;
     }
+
 }
